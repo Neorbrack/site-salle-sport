736d1bddff157ee231b33e9ea9e87a08b1c8407c
Neor <artiom_59@hotmail.fr>
2023-12-07 12:20:12 +0100

    6

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+{
+    return view('welcome');
+}
+
+}
+
+
+{
+    function  AccueilController() 
+{
+
+    return view('accueil');
+}
+}
+
+
+{
+    function  CoursController() 
+{
+
+    return view('cours');
+}
+}
+
+{
+    function  Tarifscontroller()
+{
+    return view('tarif');
+}
+}
+
+
+{
+    function  ContactController() 
+{
+    return view('contact');
+}
+}
+
+
+{
+    function  MentionsLegalesController() 
+{
+    return view('mentions-legales');
+}
+}
+
+
+
+
+
