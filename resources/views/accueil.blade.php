41e0d7776bb4ab46a50120e5d1dae5b8e26839ea
Neor <artiom_59@hotmail.fr>
2023-12-08 12:34:16 +0100

    21

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 
 <h2>Accueil</h2>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit totam quas similique incidunt aut perspiciatis quisquam enim rem nemo praesentium odio corporis laboriosam molestiae ipsum sit, voluptatem voluptatum suscipit nisi!
  <img class="tailleimage" src="image/cathy-pham-3jAN9InapQI-unsplash.jpg" alt="image de cathy pham">
  <img class="tailleimage" src="image/samuel-girven-VJ2s0c20qCo-unsplash.jpg" alt="image de samuel girven">
 @endsection
