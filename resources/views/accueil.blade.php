9e6ea1dac2077c568a754e46ea093aa2f0a5480f
Neor <artiom_59@hotmail.fr>
2023-12-08 16:08:29 +0100

    24

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -19,7 +19,7 @@
     Que vous soyez débutant ou un athlète chevronné, notre salle de sport est conçue pour vous inspirer, vous défier et vous transformer. 
     Relevez le défi, devenez alpha, et sculptez la meilleure version de vous-même chez OnlyAlpha."
 </p>
-<nav>
+<nav class="nav">
     </p>    
         <img class="tailleimage" src="image/cathy-pham-3jAN9InapQI-unsplash.jpg" alt="image de cathy pham">
         <img class="tailleimage" src="image/samuel-girven-VJ2s0c20qCo-unsplash.jpg" alt="image de samuel girven">
